cdfebf2a408edfc11633052e5cc2c375b5ed9517
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-03-06 22:05:41 -0300

    3er Commit

--- app/conexion.php
+++ app/conexion.php
@@ -1,14 +1,15 @@
 <?php
-
 // Datos de conexión a la base de datos
 $hostname = "roundhouse.proxy.rlwy.net";
 $username = "root";
 $password = "FfgBbhdC14-d3g5DDA6F5fec43cHBf3f";
 $database = "railway";
 $dbport = "21765";
+$database_url = "mysql://root:FfgBbhdC14-d3g5DDA6F5fec43cHBf3f@mysql.railway.internal:3306/railway";
+
 
 // Conexión a la base de datos
-$conn = new mysqli($hostname, $username, $password, $database, $dbport);
+$conn = new mysqli($hostname, $username, $password, $database, $dbport, $database_url);
 
 // Verificar la conexión
 if ($conn->connect_error) {
