f06086d4b30c17ce4e2d12627f43659588ca8801
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-03-06 22:11:57 -0300

    3er Commit

--- app/conexion.php
+++ app/conexion.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 // Datos de conexión a la base de datos
 $hostname = "roundhouse.proxy.rlwy.net";
 $username = "root";
