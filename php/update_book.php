88bc8afc828e0ad7637e710b8dea6e76abc46833
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-03-08 15:55:44 -0300

    Dashboard

--- php/update_book.php
+++ php/update_book.php
@@ -24,41 +24,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $titulo = $_POST['titulo'];
     $autor = $_POST['autor'];
     $sinopsis = $_POST['sinopsis'];
-    $rutaPortadaActual = $_POST['ruta_portada']; // Obtener la ruta de la portada actual desde el formulario
-
-    if (isset($_FILES['portada']) && $_FILES['portada']['error'] === UPLOAD_ERR_OK) {
-    // Obtener el nombre del archivo cargado
-    $nombreArchivo = $_FILES['portada']['name'];
-
-    // Generar un nombre único para el archivo
-    $nombreUnico = uniqid() . '.png';
-
-    // Mover el archivo cargado a una ubicación adecuada en el servidor con el nombre único
-    $rutaArchivo = '../php/portada_libros/' . $nombreUnico;
-    if (move_uploaded_file($_FILES['portada']['tmp_name'], $rutaArchivo)) {
-        // Actualizar la portada del libro en la base de datos
-        $sqlPortada = "UPDATE libros SET imagen_portada = ? WHERE id_libro = ?";
-        $stmtPortada = $conn->prepare($sqlPortada);
-        $stmtPortada->bind_param("si", $nombreUnico, $idLibro);
-        if (!$stmtPortada->execute()) {
-            // Si hay un error al actualizar la portada, mostrar el mensaje de error
-            echo "Error al actualizar la portada del libro: " . $stmtPortada->error;
-            exit();
-        }
-    } else {
-        // Error al mover el archivo de la portada
-        echo "Error al subir la portada del libro.";
-        exit();
-    }
-} else {
-    // Si no se cargó una nueva portada, mantener la ruta de la portada actual en la base de datos
-    $nombreUnico = $rutaPortadaActual;
-}
 
     // Preparar la consulta SQL para actualizar los datos del libro
-    $sqlDatos = "UPDATE libros SET titulo = ?, autor = ?, sinopsis = ?, ruta_portada = ? WHERE id_libro = ?";
+    $sqlDatos = "UPDATE libros SET titulo = ?, autor = ?, sinopsis = ? WHERE id_libro = ?";
     $stmtDatos = $conn->prepare($sqlDatos);
-    $stmtDatos->bind_param("ssssi", $titulo, $autor, $sinopsis, $rutaArchivo, $idLibro); // Cambiar $nombreUnico a $rutaArchivo
+    $stmtDatos->bind_param("sssi", $titulo, $autor, $sinopsis, $idLibro);
     if (!$stmtDatos->execute()) {
         // Si hay un error al actualizar los datos del libro, mostrar el mensaje de error
         echo "Error al actualizar los datos del libro: " . $stmtDatos->error;
@@ -68,12 +38,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Si todo fue exitoso, mostrar "success"
     echo "success";
 
-    // Cerrar las declaraciones
+    // Cerrar la declaración
     $stmtDatos->close();
-    if (isset($stmtPortada)) {
-        $stmtPortada->close();
-    }
 } else {
     // Si no se recibieron datos del formulario, mostrar un mensaje de error
     echo "Error: No se recibieron datos del formulario de edición";
 }
+?>
