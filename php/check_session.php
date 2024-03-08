5cab4318f5c76e4a28bc9b1bdf70b68ee88fe7f6
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-03-08 04:56:22 -0300

    Test

--- php/check_session.php
+++ php/check_session.php
@@ -1,6 +1,10 @@
 <?php
 session_start();
 
+header("Access-Control-Allow-Origin: http://192.168.1.10:8080");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 $response = array();
 
 if (isset($_SESSION['username'])) {
